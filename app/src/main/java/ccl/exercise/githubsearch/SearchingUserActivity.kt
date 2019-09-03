0ef215fb2f01dd2edc284a458a9336c7645c24fb
Chenling <deanli72@gmail.com>
2019-09-03 19:52:12 +0800

    init project

---
+++ app/src/main/java/ccl/exercise/githubsearch/SearchingUserActivity.kt
@@ -0,0 +1,12 @@
+package ccl.exercise.githubsearch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchingUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
